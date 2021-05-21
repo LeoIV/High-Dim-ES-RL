91a2f648f7e2fc27ecef1d1f0364bedbf60831a3
LeoIV <leonard.papenmeier@posteo.de>
2021-05-21 11:12:45 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="high-dim-es-rl",
-    version="0.0.3",
-    packages="high_dim_es_rl",
+    version="0.0.4",
+    packages=["high_dim_es_rl"],
     install_requires=["numpy",
                       "matplotlib",
                       "scipy",
