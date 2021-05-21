1e120a683ea2d7c95d6e37f444f1576ec3564d3d
LeoIV <leonard.papenmeier@posteo.de>
2021-05-21 11:23:26 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="high-dim-es-rl",
-    version="0.0.4",
+    version="0.0.5",
     packages=["high_dim_es_rl"],
     install_requires=["numpy",
                       "matplotlib",
                       "scipy",
                       "tensorflow",
-                      "keras",
+                      "keras>=2.4.3",
                       "gym"],
 )
